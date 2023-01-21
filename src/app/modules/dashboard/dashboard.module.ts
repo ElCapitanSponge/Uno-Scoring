ad5bacf183717dd83386ed0c39229c43c909617d
Andrew Brunker <andrew@brunker.net.au>
2023-01-22 09:28:54 +1100

    Fleshout the dashboard display
    Signed-off-by: Andrew Brunker <andrew@brunker.net.au>

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-
-
+import { PanelModule } from 'primeng/panel'
+import { ButtonModule } from 'primeng/button'
+import { TableModule } from 'primeng/table'
 @NgModule({
   declarations: [
     DashboardComponent
   ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    PanelModule,
+    ButtonModule,
+    TableModule
   ]
 })
 export class DashboardModule { }
