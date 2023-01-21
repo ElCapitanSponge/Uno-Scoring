30d90c600434f0b25ec4e0a5fa4d5f5d07837e32
Andrew Brunker <andrew@brunker.net.au>
2023-01-21 17:26:58 +1100

    Initialise services, interfaces, utilities and modules
    Signed-off-by: Andrew Brunker <andrew@brunker.net.au>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+  {
+    path: 'people',
+    loadChildren: () => import('./modules/people/people.module').then(m => m.PeopleModule)
+  },
+  {
+    path: 'games',
+    loadChildren: () => import('./modules/games/games.module').then(m => m.GamesModule)
+  },
+  {
+    path: 'settings',
+    loadChildren: () => import('./modules/settings/settings.module').then(m => m.SettingsModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    loadChildren: () => import('./modules/not-found/not-found.module').then(m => m.NotFoundModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
