6042ed4b3cf37b47eb51d96f38b3f5dbbcb8e7d0
Andrew Brunker <andrew@brunker.net.au>
2023-01-22 18:51:52 +1100

    Add routing for card manipulations for the game types
    Signed-off-by: Andrew Brunker <andrew@brunker.net.au>

--- src/app/modules/settings/settings-routing.module.ts
+++ src/app/modules/settings/settings-routing.module.ts
@@ -4,6 +4,9 @@ import { CreateComponent } from './components/types/create/create.component';
 import { EditComponent } from './components/types/edit/edit.component';
 import { ListComponent } from './components/types/list/list.component';
 import { ViewComponent } from './components/types/view/view.component';
+import { CreateComponent as CardCreateComponent } from './components/card/create/create.component';
+import { ViewComponent as CardViewComponent } from './components/card/view/view.component';
+import { EditComponent as CardEditComponent } from './components/card/edit/edit.component';
 
 const routes: Routes = [
   {
@@ -25,7 +28,42 @@ const routes: Routes = [
             component: ViewComponent
           }, {
             path: 'edit',
-            component: EditComponent
+            children: [
+              {
+                path: '',
+                pathMatch: 'full',
+                component: EditComponent
+              }, {
+                path: 'card',
+                children: [
+                  {
+                    path: 'create',
+                    component: CardCreateComponent
+                  }, {
+                    path: ':card_id',
+                    children: [
+                      {
+                        path: '',
+                        pathMatch: 'full',
+                        component: CardViewComponent
+                      }, {
+                        path: 'edit',
+                        component: CardEditComponent
+                      }, {
+                        path: '**',
+                        loadChildren: () => import('../not-found/not-found.module').then(m => m.NotFoundModule)
+                      }
+                    ]
+                  }, {
+                    path: '**',
+                    loadChildren: () => import('../not-found/not-found.module').then(m => m.NotFoundModule)
+                  }
+                ]
+              }, {
+                path: '**',
+                loadChildren: () => import('../not-found/not-found.module').then(m => m.NotFoundModule)
+              }
+            ]
           }, {
             path: '**',
             loadChildren: () => import('../not-found/not-found.module').then(m => m.NotFoundModule)
