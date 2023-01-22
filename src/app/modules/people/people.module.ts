3bfe146a0f201ab52bef5cd6a72ee770af1187fc
Andrew Brunker <andrew@brunker.net.au>
2023-01-22 18:46:53 +1100

    Update the layout of the editing and viewing of already configured components
    Signed-off-by: Andrew Brunker <andrew@brunker.net.au>

--- src/app/modules/people/people.module.ts
+++ src/app/modules/people/people.module.ts
@@ -1,18 +1,17 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { PeopleRoutingModule } from './people-routing.module';
-import { ListComponent } from './components/list/list.component';
-import { CreateComponent } from './components/create/create.component';
-import { EditComponent } from './components/edit/edit.component';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
+import { FieldsetModule } from 'primeng/fieldset';
+import { InputTextModule } from 'primeng/inputtext';
 import { PanelModule } from 'primeng/panel';
 import { RippleModule } from 'primeng/ripple';
 import { TableModule } from 'primeng/table';
-import { InputTextModule } from 'primeng/inputtext';
-import { FormsModule } from '@angular/forms';
+import { CreateComponent } from './components/create/create.component';
+import { EditComponent } from './components/edit/edit.component';
+import { ListComponent } from './components/list/list.component';
 import { ViewComponent } from './components/view/view.component';
-
+import { PeopleRoutingModule } from './people-routing.module';
 
 @NgModule({
   declarations: [
@@ -29,7 +28,8 @@ import { ViewComponent } from './components/view/view.component';
     TableModule,
     RippleModule,
     InputTextModule,
-    FormsModule
+    FormsModule,
+    FieldsetModule
   ]
 })
 export class PeopleModule { }
--- src/app/modules/settings/settings.module.ts
+++ src/app/modules/settings/settings.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { SettingsRoutingModule } from './settings-routing.module';
-import { ListComponent } from './components/types/list/list.component';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { FieldsetModule } from 'primeng/fieldset';
+import { InputTextModule } from 'primeng/inputtext';
+import { PanelModule } from 'primeng/panel';
+import { RippleModule } from 'primeng/ripple';
+import { TableModule } from 'primeng/table';
 import { CreateComponent } from './components/types/create/create.component';
 import { EditComponent } from './components/types/edit/edit.component';
-import { RippleModule } from 'primeng/ripple';
-import { ButtonModule } from 'primeng/button';
-import { PanelModule } from 'primeng/panel';
-import { TableModule } from 'primeng/table';
-import { InputTextModule } from 'primeng/inputtext';
+import { ListComponent } from './components/types/list/list.component';
 import { ViewComponent } from './components/types/view/view.component';
-
+import { SettingsRoutingModule } from './settings-routing.module';
 
 @NgModule({
   declarations: [
@@ -27,7 +27,9 @@ import { ViewComponent } from './components/types/view/view.component';
     ButtonModule,
     TableModule,
     RippleModule,
-    InputTextModule
+    InputTextModule,
+    FieldsetModule,
+    FormsModule
   ]
 })
 export class SettingsModule { }
